c0fada156a77ad364b6428853bda3eafaf245088
Aman Kumar Singh <123794954+amansingh4012@users.noreply.github.com>
2025-09-24 04:31:48 +0530

    commit for deployment

--- BACKEND/index.js
+++ BACKEND/index.js
@@ -16,9 +16,14 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
+// const corsOptions = {
+//     origin: 'http://localhost:5173',
+//     credentials:true
+// }
+
 const corsOptions = {
-    origin: 'http://localhost:5173',
-    credentials:true
+    origin: process.env.FRONTEND_URL,
+    credentials: true
 }
 
 app.use(cors(corsOptions));
