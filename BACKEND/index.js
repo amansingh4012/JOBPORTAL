55d5673548dfe3855aa3ae8765373448838629db
Aman Kumar Singh <123794954+amansingh4012@users.noreply.github.com>
2025-09-24 03:11:37 +0530

    commit

--- BACKEND/index.js
+++ BACKEND/index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-    origin: https://jobportal-frontend-five.vercel.app,
+    origin: jobportal-frontend-five.vercel.app,
     credentials:true
 }
 
