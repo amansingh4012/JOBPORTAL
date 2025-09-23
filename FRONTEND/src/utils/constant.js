4677f791388afd3b939c9869ea17f70e35825e9d
Aman Kumar Singh <123794954+amansingh4012@users.noreply.github.com>
2025-09-24 03:35:05 +0530

    commit

--- FRONTEND/src/utils/constant.js
+++ FRONTEND/src/utils/constant.js
@@ -4,7 +4,7 @@
 // export const COMPANY_API_END_POINT="http://localhost:8000/api/v1/company";
 
 
-const API_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:8000";
+const API_URL = 'https://jobportal-backend-xdq3todla-aman-kr-singhs-projects.vercel.app';
 export const USER_API_END_POINT = `${API_URL}/api/v1/user`;
 export const JOB_API_END_POINT = `${API_URL}/api/v1/job`;
 export const APPLICATION_API_END_POINT = `${API_URL}/api/v1/application`;
