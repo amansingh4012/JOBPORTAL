e9446efe7f5fec0b39d57aef1dcbcbaa31efb946
Aman Kumar Singh <123794954+amansingh4012@users.noreply.github.com>
2025-09-24 18:04:00 +0530

    commit db.js

--- BACKEND/utils/db.js
+++ BACKEND/utils/db.js
@@ -2,7 +2,8 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI);
+       // await mongoose.connect(process.env.MONGO_URI);
+        await mongoose.connect('mongodb+srv://JOBPORTAL:JOBPORTAL@cluster0.a2dvbx1.mongodb.net/');
         console.log('mongodb connected successfully');
     } catch (error) {
         console.log(error);
